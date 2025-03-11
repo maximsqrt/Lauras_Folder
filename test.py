8738c723954113ed9f8d9282cae9bec225ab4ff4
Maxim <maxim@mail.de>
2025-03-11 11:34:03 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,7 @@
+#echo "# Lauras_Folder" >> README.md
+#git init
+#git add . (wir wollen alles laden bis auf durch .gitignore ausgeschlossen)
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/maximsqrt/Lauras_Folder.git
+#git push -u origin main
